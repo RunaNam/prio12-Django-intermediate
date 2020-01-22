67279c2a2398bacee665b073af04c774d9dc6807
Nam Minjeong <nmj5012@naver.com>
2020-01-22 18:22:23 +0900

    Ep_03

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+@login_required
 def profile(request):
+    return render(request, 'accounts/profile.html')
