89099ab716c576458817afcf58b96d59540aa47a
Nam Minjeong <nmj5012@naver.com>
2020-01-23 12:28:18 +0900

    Ep_11

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name= 'signup'),
+
+    path('login/url/', views.RequestLoginViaUrlView.as_view(), name='request_login_via_url'),
+    path('login/<uidb64>/<token>/', views.login_via_url, name='login_via_url'),
+
 ]
